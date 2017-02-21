2ad5c7aefe9c19b2f4827b9710c43405b7c28a12
Nguyen Hoang Minh <minhnguyendhth8b@gmail.com>
2017-02-21 18:13:59 +0700

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
